5958af272e8cd4d33d61da326af916d8f0553329
kamal-suryavanshi <33751904+kamal-suryavanshi@users.noreply.github.com>
2024-02-28 19:30:46 +0530

    get filtered jobs

--- src/redux/postJob/actionCreator.js
+++ src/redux/postJob/actionCreator.js
@@ -1,3 +1,4 @@
+import { message } from 'antd';
 import actions from './actions';
 import { DataService } from '../../config/dataService/dataService';
 import initialState from '../../demoData/jobPosts.json';
@@ -13,8 +14,10 @@ const submitPost = (data) => {
 
       if (response.data.success) {
         dispatch(jobPostDataSuccess(data));
+        message.success('Job Post Successfull');
       } else {
         dispatch(jobPostDataErr(response.data.message));
+        message.error('Job Post Failed');
       }
     } catch (err) {
       dispatch(jobPostDataErr(err));
@@ -22,14 +25,19 @@ const submitPost = (data) => {
   };
 };
 
-const getAllJobs = () => {
+const getAllJobs = (filter) => {
   return async (dispatch) => {
     try {
       dispatch(getAllJobsBegin());
-      if (initialState) {
+      if (initialState && filter) {
         dispatch(getAllJobsSuccess(initialState));
+        message.success('Get Filtered Jobs Successfull');
+      } else if (initialState) {
+        dispatch(getAllJobsSuccess(initialState));
+        message.success('Get All Jobs Successfull');
       } else {
         dispatch(getAllJobsErr(null));
+        message.error('Get Job Post Failed');
       }
     } catch (err) {
       dispatch(getAllJobsErr(err));
